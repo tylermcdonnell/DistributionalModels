057d7ebcf9740b409d421006e7d805b63f82526d
Tyler McDonnell <tyler@cs.utexas.edu>
2016-04-28 18:58:59 -0500

    Updated WordMap. Renamed map methods.

--- wordmap.py
+++ wordmap.py
@@ -1,26 +1,60 @@
+'''
+@author: Tyler McDonnell / http://tylermcdonnell.com
+'''
+
+# Standard
 import pickle
+import anydbm
 
 class WordMap(object):
+    '''
+    Maps words to integers for space efficiency and/or array transformation: e.g., 
+    sparse feature vectors may more efficiently be described as dicts of features
+    which can be converted to mapped arrays on demand.
+    '''
     
     def __init__(self):
         self.count = 0
-        self.map   = {}
-        self.rmap  = {}
+        self.wmap  = {}
+        self.rwmap = {}
 
-    def apply(self, word):
-        if word not in self.map:
-            self.map.update({ word : self.count })
-            self.rmap.update({ str(self.count) : word })
+    def wmap(self, word):
+        '''
+        Returns the integer mapping for this word. If this word is not in the
+        WordMap, an integer mapping will be created for it.
+        '''
+        if word not in self.wmap:
+            self.wmap.update({ word : str(self.count) })
+            self.rwmap.update({ str(self.count) : word })
             self.count += 1
-        return self.map[word]
+        return int(self.wmap[word])
 
-    def demap(self, mapped_word):
-        return self.rmap[str(mapped_word)]
+    def dewmap(self, map_id):
+        '''
+        Returns the word for this integer mapping. If this word is not in the
+        WordMap, returns None.
+        '''
+        return self.rwmap[map_id] if map_id in self.rwmap else None
 
     def save(self, filename):
-        pickle.dump(self, open(filename, 'wb'))
+        '''
+        Saves this wordmap to disk.
+        '''
+        print ("Saving WordMap to disk: %s" % filename)
+        db = anydbm.open(filename, 'c')
+        for key in self.wmap:
+            db[key] = self.wmap[key]
+        print ("Finished saving WordMap to disk.")
+        db.close()
     
     @classmethod
     def load(filename):
-        return pickle.load(filename)
+        print ("Loading WordMap: %s" % filename)
+        loaded = WordMap()
+        db = anydbm.open(filename, 'c')
+        for key in db:
+            self.wmap.update( { key : db[key] })
+            self.rwmap.update( { db[key] : key })
+        db.close()
+        print ("Loaded %d words into WordMap." % len(self.wmap))
         
