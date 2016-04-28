9f6e45d70079f72dc456e0bdc0bf3280044e90aa
Tyler McDonnell <tyler@cs.utexas.edu>
2016-04-28 11:45:03 -0500

    Added basic count model.

--- distributional.py
+++ distributional.py
@@ -10,6 +10,7 @@ from abc import abstractmethod
 from collections import Counter, defaultdict, namedtuple
 # NLTK
 import nltk
+import nltk.data
 from nltk.stem.snowball import SnowballStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
 ##################################################################
@@ -20,22 +21,11 @@ class DistributionalModel(object):
    
     A skeleton for building distributional models of semantic meaning.
 
-    The Vocabulary is represented as a dictionary of { word : features }. Distributional
-    models the feature vector by the contexts the word appears in. The resultant vectors
-    are generally assumed to be sparse, which is why we choose to explicitly map features
-    in a dictionary, rather than an array.
+    In a distributional model, each word in a vocabulary V is represented by an array of
+    features, where each feature is derived from the contexts in which that word appears.
+    The resultant vectors are generally sparse, so we choose to model them here as dicts,
+    where each feature has a string label. 
     '''
-
-    @abstractmethod
-    def train(self, text, preprocessing_filters=None, token_filters=None, wordmap=None):
-        '''
-        Trains the model on the input data.
-        
-        preprocessing_filters -- Filters to apply to input text before processing.
-        token_filters         -- Filters to exclude tokens from feature vector creation.
-        wordmap               -- WordMap for storage efficiency.
-        '''
-        pass
 
     def train_on_multiple(self, files, preprocessing_filters=None, token_filters=None):
         '''
@@ -47,14 +37,48 @@ class DistributionalModel(object):
         '''
         count = 0
         for file in files:
+            tokenizer = nltk.data.load('tokenizers/punkt/english.pickle') 
             count += 1
             print ("Training model on %d of %d: %s" % (count, len(files), file))
-            try:
-                raw = open(file, 'rb').read()
-                text = nltk.word_tokenize(raw)
-                self.train(text, preprocessing_filters, token_filters)
-            except:
-                print ("Aborted training on %s - Unicode Error." % file)
+            #try:
+            raw = open(file, 'rb').read()
+            sentences = tokenizer.tokenize(raw)
+            text = [nltk.word_tokenize(s) for s in sentences]
+            self.train(text, preprocessing_filters, token_filters)
+            #except:
+            #    print ("Aborted training on %s - Unicode Error." % file)
+
+    def _preprocessing(self, text, preprocessing_filters=None):
+        '''
+        Applies the preprocessing filters to the text in the order provided.
+        
+        text                  -- List of tokenized sentences.
+        preprocessing_filters -- Filters to apply.
+        '''
+        processed = []
+        for ppf in preprocessing_filters:
+            for sentence in text:
+                processed_sentence = ppf.apply(sentence)
+                processed.append(processed_sentence)
+        return processed
+
+    def _token_desired(self, token, token_filters):
+        for tf in token_filters:
+            if token not in tf.apply([token]):
+                return False
+        return True
+
+    @abstractmethod
+    def train(self, text, preprocessing_filters=None, token_filters=None, wordmap=None):
+        '''
+        Trains the model on the input data.
+        
+        text                  -- List of tokenized sentences.
+        preprocessing_filters -- Filters to apply to input text before processing.
+        token_filters         -- Filters to exclude tokens from feature vector creation.
+        wordmap               -- WordMap for storage efficiency.
+        '''
+        pass
 
     @abstractmethod
     def feature_vectors(self):
@@ -90,11 +114,12 @@ class StandardModel(DistributionalModel):
 
     def train(self, text, preprocessing_filters=None, token_filters=None, wordmap=None):
         text = self._preprocessing(text, preprocessing_filters)
-        for w in window(text, self.windowsize):
-            word    = w.word
-            context = w.context
-            if self._token_desired(word, token_filters):
-                self.model[word].update(context)
+        for sentence in text:
+            for w in window(sentence, self.windowsize):
+                word    = w.word
+                context = w.context
+                if self._token_desired(word, token_filters):
+                    self.model[word].update(context)
 
     def feature_vectors(self):
         return self.model
@@ -107,17 +132,6 @@ class StandardModel(DistributionalModel):
         db = BerkeleyStore(filename)
         for word in sorted(db.keys()):
             self.model.update( { word : db.context(word) })
-
-    def _preprocessing(self, text, preprocessing_filters):
-        for ppf in preprocessing_filters:
-            text = ppf.apply(text)
-        return text
-
-    def _token_desired(self, token, token_filters):
-        for tf in token_filters:
-            if token not in tf.apply([token]):
-                return False
-        return True
 
     def _clear(self):
         '''
@@ -137,6 +151,46 @@ def window(seq, size):
         context = seq[max(i - size, 0):i] + \
                   seq[min(i + 1, len(seq)):min(i + size, len(seq))]
         yield Context(word = seq[i], context = context)
+
+
+
+class SimpleDistribution(DistributionalModel):
+    '''
+    Simple 1-gram probability distribution of tokens in model.
+    '''
+    
+    def __init__(self):
+        self.counts = defaultdict(lambda: defaultdict(int))
+        self.total  = 0
+    
+    def train(self, text, preprocessing_filters=None, token_filters=None, wordmap=None):
+        text = self._preprocessing(text, preprocessing_filters)
+        for sentence in text:
+            for token in sentence:
+                self.total += 1
+                if self._token_desired(token, token_filters):
+                    self.counts[token]['#F_WordCount'] += 1
+
+    def feature_vectors(self):
+        return self.counts
+
+    def save(self, filename):
+        db = BerkeleyStore(filename)
+        db.update(self.counts)
+
+    def load(self, filename):
+        db = BerkeleyStore(filename)
+        for word in sorted(db.keys()):
+            self.counts.update( { word : db.context(word) })
+
+
+
+class PartOfSpeechModel(DistributionalModel):
+    '''
+    The Part-of-Speech model is a variant of the traditional distributional model which,
+    for a given window size N, selects the nearest N words of the specified POS within
+    the sentence boundary. 
+    '''
 
 
 
